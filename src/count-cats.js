41baa3c0699f40b983016334525ba889912b23b2
German <pentagram777@mail.ru>
2020-10-15 08:55:12 +0500

    'feat: add cat solutions'

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  
+    let res = 0
+    for (const cat of matrix) {
+      for (const ear of cat) {
+      if (ear === "^^") {
+        res += 1
+      }
+     }
+  
+  }
+    return res
+}
+
