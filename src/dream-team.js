5d20feb630f6bdabdf59b07408382e7c6e1bc558
German <pentagram777@mail.ru>
2020-10-15 09:25:22 +0500

    'feat: add solution dream team'

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let res = ''
+  if (Array.isArray(members) === false){
+    return false
+  } 
+  for (const name of members) {
+    if (typeof(name) === 'string'){
+      console.log(name.replace(/ /g, '').slice(0, 1))
+    res += name.replace(/ /g, '').slice(0, 1)
+    }
+  }
+  
+  return res.toUpperCase().split('').sort().join('')
 };
